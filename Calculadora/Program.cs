7ba5531e916c83160e84558ec93c7e34b0c8c7d9
itzandymonegro@gmail.com <itzandymonegro@gmail.com>
2020-11-19 17:34:19 -0400

    Carga inicial

---
+++ Calculadora/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingresar solo números enteros ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            if (numero > 0)
+            {
+                String cadena = "";
+                while (numero > 0)
+                {
+                    if (numero % 2 == 0)
+                    {
+                        cadena = "0" + cadena;
+                    }
+                    else
+                    {
+                        cadena = "1" + cadena;
+                    }
+                    numero = (int)(numero / 2);
+                }
+                Console.WriteLine(cadena);
+            }
+            else
+            {
+                if (numero == 0)
+                {
+                    Console.WriteLine("0");
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese solo numeros positivos");
+                }
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
